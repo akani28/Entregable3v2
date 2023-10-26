663c52aa9ea813d88e2aab7985ee2c5028641e9d
akani28 <nietocevallos61@gmail.com>
2023-10-26 00:22:00 -0500

    modified files

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,61 @@
-
-import { useEffect, useState } from 'react'
-import './App.css'
-import Location from './components/Location'
-import axios from 'axios'
-import { getRandomLocation } from './randomLocation'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Location from "./components/Location";
+import axios from "axios";
+import { getRandomLocation } from "./randomLocation";
 
 function App() {
-  const [location, setLocation] = useState(null)
-  const getInformation=(url)=>{
+  const [location, setLocation] = useState(null);
+  const [idLocation, setIdLocation] = useState("");
 
+  const handleOnChange = (e) => {
+    setIdLocation(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  const handleClick = () => {
+    getInformation(idLocation);
+  };
+
+  const getInformation = (location) => {
     axios
-  .get(url)
-  .then(({data})=>{
-    setLocation(data)
-    console.log(data)
-  })
-  .catch((err)=>console.log(err))
+      .get(`https://rickandmortyapi.com/api/location/${location}`)
+      .then(({ data }) => {
+        setLocation(data);
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  };
+  const URL = `https://rickandmortyapi.com/api/location/${getRandomLocation()}`;
 
-
-  }
-  const URL=`https://rickandmortyapi.com/api/location/${getRandomLocation()}`
-
-useEffect(()=>{
- getInformation(URL)
-
-}, [])
-  
-  
+  useEffect(() => {
+    getInformation(getRandomLocation());
+  }, []);
 
   return (
     <>
-    <main className='bg-[#000000] p-4'>
-    <Location location={location}/>
-    </main>
-   
+      <main className="bg-[#000000] p-4">
+        
+        <form className="flex m-3 text-center border-green-500 border-2 " onSubmit={handleSubmit}>
+          <input className="w-3/4 p-1 outline-none bg-black text-gray-400 placeholder:text-center" value={idLocation} onChange={handleOnChange} placeholder="Type a location id..." type="text" />
+          <button className="bg-green-400 w-1/4 p-2" onClick={handleClick}>
+          <svg
+            className="inline"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z" />
+            <path d="M11.412 8.586c.379.38.588.882.588 1.414h2a3.977 3.977 0 0 0-1.174-2.828c-1.514-1.512-4.139-1.512-5.652 0l1.412 1.416c.76-.758 2.07-.756 2.826-.002z" />
+          </svg>
+          </button>
+        </form>
+        <Location location={location} setLocation={setLocation} />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ResidentCard from "./ResidentCard";
+import axios from "axios";
 
 const Location = ({ location }) => {
   return (
@@ -14,29 +15,11 @@ const Location = ({ location }) => {
           <span className="p-1">Population: {location?.residents.length}</span>
         </section>
       </main>
-      <div className="flex m-3 text-center border-green-500 border-2 bg-green-400">
-        <input
-          className="p-1 outline-none bg-black text-gray-400 placeholder:text-center"
-          type="text"
-          name="location"
-          placeholder="Type a location id..."
-        />
-        <label className=" w-full p-1 hover:cursor-pointer" htmlFor="location">
-          <svg
-            className="inline"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-          >
-            <path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z" />
-            <path d="M11.412 8.586c.379.38.588.882.588 1.414h2a3.977 3.977 0 0 0-1.174-2.828c-1.514-1.512-4.139-1.512-5.652 0l1.412 1.416c.76-.758 2.07-.756 2.826-.002z" />
-          </svg>
-        </label>
-      </div>
+
       <div>
-        {location?.residents.map((resident, index) => (<div key={index}>
-            <ResidentCard residentLocation={resident}  />
+        {location?.residents.map((resident) => (
+          <div key={resident}>
+            <ResidentCard residentLocation={resident} />
           </div>
         ))}
       </div>
